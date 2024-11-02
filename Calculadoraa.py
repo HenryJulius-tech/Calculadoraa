68101fb3d804ea580a1946e1118e29db9d0be371
HenryJulius-tech <henryjuliuscamargocorrea@gmail.com>
2024-11-02 11:48:36 -0500

    Revert "Operacion multiplicacion agregada"

--- Calculadoraa.py
+++ Calculadoraa.py
@@ -6,7 +6,7 @@
 #Brayan Amaya
 def suma(num1, num2):
     
-    return num1 + num2 
+    pass  
 
 def resta(num1, num2):
     
@@ -14,7 +14,7 @@ def resta(num1, num2):
 
 def multiplicacion(num1, num2):
     
-    return num1 * num2
+    pass  
 
 def division(num1, num2):
 
@@ -37,13 +37,3 @@ operacion = input("Ingrese la operación deseada: ").lower()
 if operacion in ["suma", "resta", "multiplicacion", "division", "potencia"]:
     num1 = int(input("Ingrese el primer número entero: "))
     num2 = int(input("Ingrese el segundo número entero: "))
-
-    # Operacion suma - Cristian Miranda
-    if operacion == "suma":
-        resultado = suma(num1, num2)
-        print("Resultado de la suma:", resultado)
-
-#Operacion multiplicacion - Marleinis Orozco
-    elif operacion == "multiplicacion":
-        resultado = multiplicacion(num1, num2)
-        print("Resultado de la multiplicacion:", resultado)
